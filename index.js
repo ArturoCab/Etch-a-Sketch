18cf4dbc949a54d682e7e2599f3151e87131430d
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-11 00:04:09 -0600

    hovering effect using javascript

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ var width=window.innerWidth/16;
 for(let i=0; i<16*16; i++){
     const d=document.createElement("div");
     d.style.border="solid 1px black";
-    d.style.width=width;
+    
+    d.addEventListener("mouseenter",mouseEnter);
+    d.addEventListener("mouseleave",mouseLeave);
+
     divs.push(d);
 }
 const container=document.querySelector(".container");
 
 divs.forEach(e=>{
     container.appendChild(e);
-})
+});
+
+function mouseEnter(event){
+    console.log(event.target);
+    event.target.style.background="cyan";
+}
+
+function mouseLeave(event){
+    event.target.style.background="white";
+}
